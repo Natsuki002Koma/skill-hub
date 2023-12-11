7e681cfb599bb6ce1a8c0741214efe554827854a
Natsuki002Koma <furusawa.n.002@gmail.com>
2023-12-11 17:36:03 +0900

    usersコントローラー、ビューファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "skills#index"
+  resources :users, only: :index
   resources :skills, only:[:index, :new, :create, :edit, :update] do
     resources :tweets, only:[:index, :create, :destroy]
   end
