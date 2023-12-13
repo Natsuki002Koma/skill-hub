9e6fb4bb3bd978713c64cc9e3090ef8f2d18bfd5
Natsuki002Koma <furusawa.n.002@gmail.com>
2023-12-13 17:51:14 +0900

    ルーティング、バリデーション、アソシエーションの設定

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
-  has_many :users, through: :project_users
   has_many :project_users
+  has_many :users, through: :project_users
   has_many :messages
 
   validates :project_name, presence: true
